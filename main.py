aa0c50038bf1cd06e5e27d4dd4f39d21a266da92
solteszistvan <solteszistvan@outlook.com>
2022-11-16 06:53:41 +0100

    Initialization

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    #Take in an argument vector
+    #Take the first parameter of the argument vector!
+    #Take the second parameter of the argument vector
+    #If the first parameter is a .py file, then transform it into a class representation, otherwise throw error.
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
